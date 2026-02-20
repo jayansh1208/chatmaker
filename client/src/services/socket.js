6bdf27fbcdb782db8f748de1016b8a7c7500fb71
jayansh1208 <jayanshjain10@gmail.com>
2026-02-20 20:41:16 +0530

    Force relative URLs in production to fix CORS

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || (import.meta.env.PROD ? undefined : 'http://localhost:5000');
+const SOCKET_URL = import.meta.env.PROD ? undefined : (import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000');
 
 class SocketService {
     constructor() {
